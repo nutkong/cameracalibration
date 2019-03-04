cfaa65e831247d293dce9ad6e593a9c6a25a1204
nutkong <nuttapon.kong@gmail.com>
2019-03-04 13:08:55 +0700

    make change to gen_tyimestamp

--- gen_timestamp.py
+++ gen_timestamp.py
@@ -3,15 +3,14 @@
 
 #it should read number of files in the directory and generate the timestamps.txt
 import os, fnmatch
-
-init_timestamp = 4001
-fps = 1000
-files_type = '*.png'
-outputfile = 'orbslam2_timestamp.txt'
+files_type = '*.jpg'
+outputfile = 'orbslam2_office1_timestamp.txt'
 
 def GenerateTimestamp(footage_dir):
+  init_timestamp = '00000.jpg'
   n_files = CountFiles(footage_dir)
-  time_step = float(1)/fps*1000
+  time_step = float(1)
+  digit_length = 5
   print time_step
   print n_files
   print init_timestamp
@@ -19,8 +18,10 @@ def GenerateTimestamp(footage_dir):
   outfile = open(str(outputfile), "w")
 
   for i in range(n_files):
-    new_timestamp = init_timestamp + i*int(time_step)
-    outfile.write('0' + str(new_timestamp) + '\n')
+    #new_timestamp = int(init_timestamp[:digit_length]) + i*int(time_step)
+    new_timestamp = ('0000' + str(int(i*time_step)))[-digit_length:]
+    print new_timestamp
+    outfile.write( str(new_timestamp) + '\n')
 
   outfile.close()
 
@@ -28,4 +29,4 @@ def CountFiles( footage_dir):
   return  len(fnmatch.filter(os.listdir( footage_dir ), files_type ))
 
 
-GenerateTimestamp('/home/nutkong/Desktop/5001_N/')
+GenerateTimestamp('/home/nutkong/Desktop/office1-color/')
